987a5a43943391e44f8187395f6868f91968aa65
6Kehan <liukehan2025@163.com>
2026-01-28 16:57:51 +0000

    feat: add env

--- backend/console/dal/rds/client.py
+++ backend/console/dal/rds/client.py
@@ -1,12 +1,25 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+# 1. 引入 dotenv
+from dotenv import load_dotenv
 
-# 格式: mysql+pymysql://用户名:密码@地址:端口/数据库名
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:lkhdsg@localhost:3306/StarRoll"
+# 2. 加载 .env 文件
+load_dotenv()
+
+# 3. 从环境变量读取 (如果没有读到，就使用默认值)
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "123456")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "3306")
+DB_NAME = os.getenv("DB_NAME", "StarRoll")
+
+# 4. 拼接连接字符串
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
-    pool_pre_ping=True, # 自动重连，防止连接断开
+    pool_pre_ping=True,
     echo=False
 )
 
