66bd413aa6ada1ff3ed604bbe9d01bec15a92a9d
dshuzz <hes2@tcd.ie>
2026-02-13 00:59:00 +0800

    task: token in request

--- frontend/vue-app/src/utils/requests.ts
+++ frontend/vue-app/src/utils/requests.ts
@@ -7,12 +7,13 @@ const request = axios.create({
 });
 
 // 请求拦截器：自动在 Header 中加入 Token
-// request.interceptors.request.use(config => {
-//   const token = localStorage.getItem('token');
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
+request.interceptors.request.use(config => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}
+);
 
 export default request;
--- frontend/vue-app/vite.config.ts
+++ frontend/vue-app/vite.config.ts
@@ -13,12 +13,12 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  // --- 新增开发服务器配置 ---
+
   server: {
     proxy: {
       // 当你代码里请求以 /api 开头的地址时
       '/api': {
-        target: 'http://10.6.50.69:8000', // 这里填你后端真实的运行地址
+        target: 'http://10.6.58.78:8000', // 这里填你后端真实的运行地址
         changeOrigin: true,              // 允许跨域
       }
     }
