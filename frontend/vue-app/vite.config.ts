c1504efab0b5a54e31ea5dfc60086fe995cd9a8a
Gore Suche <goresuche@Gores-MacBook-Air.local>
2026-02-19 17:22:31 +0000

    login

--- frontend/vue-app/vite.config.ts
+++ frontend/vue-app/vite.config.ts
@@ -18,8 +18,10 @@ export default defineConfig({
     proxy: {
       // 当你代码里请求以 /api 开头的地址时
       '/api': {
-        target: 'http://10.6.58.78:8000', // 这里填你后端真实的运行地址
+        target: 'http://localhost:8000', // 这里填你后端真实的运行地址
         changeOrigin: true,              // 允许跨域
+        rewrite: (path) => path.replace(/^\/api/, ''), // 去掉 /api 前缀（如果后端路由没有）
+        secure: false, // 若后端是自签名 https 可启用
       }
     }
   }
