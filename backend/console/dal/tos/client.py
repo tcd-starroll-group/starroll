1da2be6f4b6a33f188bdd1c21a94dfe26a49343b
sahilsabir <48446007+sahilsabir@users.noreply.github.com>
2026-02-23 14:25:43 +0000

    Get star identify job result  (#38)
    * job result api- handover to huafu
    
    * star identify job result api completed
    
    * build error fix

--- backend/console/dal/tos/client.py
+++ backend/console/dal/tos/client.py
@@ -24,7 +24,6 @@ def get_minio_client() -> Minio:
         settings.minio_endpoint,
         access_key=settings.minio_access_key,
         secret_key=settings.minio_secret_key,
-        secure=settings.minio_secure,
     )
 
 
